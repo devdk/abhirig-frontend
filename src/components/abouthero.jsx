9c9c616c3f1ba58089b2562fdcc01b9a3cebffec
Anmol Arora <heymailme04@gmail.com>
2024-05-03 19:29:49 +0530

    Resolved the broken Logo Img

--- src/components/abouthero.jsx
+++ src/components/abouthero.jsx
@@ -44,7 +44,7 @@ export default function Abouthero() {
             ref={logoRef}
             className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" 
             alt="hero" 
-            src="./src/assets/logo.png"
+            src="./assets/logo.png"
           />
           <div className="text-center lg:w-2/3 w-full">
             <h1 className="text-transparent font-semibold mb-8 bg-clip-text bg-gradient-to-r lg:text-4xl  to-gold-100 from-yellow-400 via-lightGreen">
